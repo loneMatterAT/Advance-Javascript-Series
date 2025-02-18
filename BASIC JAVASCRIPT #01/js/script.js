f5dc64b1f7c4e14871ca734f7873537ab7758577
loneMatterAT <luxuryacec@gmail.com>
2025-02-18 23:07:35 +0800

    This is my Javascript #1 Series

---
+++ BASIC JAVASCRIPT #01/js/script.js
@@ -0,0 +1,22 @@
+// BASIC JAVASCRIPT 01
+
+// Console log
+console.log(`Hello`);
+console.log(`I like pizza!`);
+
+// Alert
+alert(`This is an alert!`);
+alert(`I like pizza!`);
+
+// Comments
+// This is a comment (Single line comment)
+
+/*
+  This
+  is a 
+  comment (Multi-line comments)
+*/
+
+// Document get element to display text on page
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`;
