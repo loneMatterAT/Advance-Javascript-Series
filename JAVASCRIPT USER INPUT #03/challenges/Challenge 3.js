f7db1a31ba0b9e90e463cc0a9c2f344e505e15a2
loneMatterAT <luxuryacec@gmail.com>
2025-02-20 18:18:32 +0800

    This is the continue of Javascript Part #3 Simple Challenge.

--- JAVASCRIPT USER INPUT #03/challenges/Challenge 3.js
+++ JAVASCRIPT USER INPUT #03/challenges/Challenge 3.js
@@ -1,1 +1,33 @@
 // JAVASCRIPT CHALLENGE 03 : USER INPUT
+
+// Empty variables
+let fullName;
+let birthDate;
+let sex;
+let address;
+let contact;
+let cStatus;
+let course;
+let school;
+let start;
+let end;
+
+document.getElementById("mySubmit").onclick = function() {
+  fullName = document.getElementById("fullname").value;
+  birthDate = document.getElementById("birthdate").value;
+  sex = document.getElementById("sex").value;
+  address = document.getElementById("address").value;
+  contact = document.getElementById("contact").value;
+  cStatus = document.getElementById("status").value;
+  course = document.getElementById("course").value;
+  school = document.getElementById("school").value;
+  start = document.getElementById("date-start").value;
+  end = document.getElementById("date-end").value;
+
+  // Bio Data Summary
+  document.getElementById("myH1").textContent = `You are hired ${fullName}!`;
+
+  alert(`BIO DATA SUMMARY\nFullname : ${fullName}\nBirthdate : ${birthDate}\nSex : ${sex}
+    \nAddress : ${address}\nContact No : ${contact}\nCivil Status : ${cStatus}\nCourse : ${course}
+    \nSchool : ${school}\nDate Started : ${start}\nDate Ended : ${end}`);
+}
